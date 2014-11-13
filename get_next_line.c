c907127e3c72628dca02cfb77aa53bbec4506c74
Bric3d <bbecker>
2014-11-13 17:13:39 +0100

    maj, working

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bbecker <bbecker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/10 10:18:17 by bbecker           #+#    #+#             */
-/*   Updated: 2014/11/12 20:12:49 by bbecker          ###   ########.fr       */
+/*   Updated: 2014/11/13 17:05:57 by bbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,12 @@ char	*ft_strjoin2(char *s1, char *s2)
 	return (data);
 }
 
+void	ft_free_all(t_infos *infos)
+{
+	free(infos->tmp);
+	free(infos);
+}
+
 int		ft_write_inline(char **line, t_infos *infos)
 {
 	unsigned int	i;
@@ -51,44 +57,18 @@ int		ft_write_inline(char **line, t_infos *infos)
 	tmp = infos->tmp;
 	size = 0;
 	i = infos->start;
-	while(tmp[i] && tmp[i++] != '\n')
+	while (tmp[i] && tmp[i++] != '\n')
 		size++;
-	ft_putnbr(infos->start);
-	ft_putendl(" ");
-	ft_putnbr(infos->count);
-	ft_putendl(" ");
-	ft_putchar(tmp[infos->start + size]);
-	line[infos->count] = ft_strsub(tmp, (unsigned int)infos->start, size);
-	ft_putendl("B");
-	if (!line[infos->count])
+	*line = ft_strsub(tmp, (unsigned int)infos->start, size);
+	if (!*line)
 		return (-1);
-	ft_putendl(line[infos->count]);
-	infos->count++;
-	infos->start = infos->start + i + 1;
-	return (0);
+	infos->start = infos->start + size + 1;
+	if (!tmp[i])
+		infos->end = 1;
+	return (1);
 }
 
-int		ft_textsize(char **line, t_infos *infos)
-{
-	int i;
-
-	infos->linenu = 0;
-	while (infos->tmp[i])
-	{
-		if (infos->tmp[i] == 10 && !infos->tmp[i + 1]
-			&& infos->tmp[i + 1] != 10)
-			infos->linenu++;
-		i++;
-	}
-	// Line ne reste pas alloue, et n'est pas transmis au autres fonctions.
-	//line = (char **)ft_memalloc(sizeof(char *) * (infos->linenu + 1));
-	//if (!line)
-	//	return (-1);
-	//line[infos->linenu] = '\0';
-	return (0);
-}
-
-int		ft_read_fd	(int const fd, t_infos *infos, char **line)
+int		ft_read_fd(int const fd, t_infos *infos)
 {
 	int		ret;
 	char	*buf;
@@ -106,8 +86,7 @@ int		ft_read_fd	(int const fd, t_infos *infos, char **line)
 			return (-1);
 	}
 	infos->state = 1;
-	if (ret >= 0)
-		ret = ft_textsize(line, infos);
+	free(buf);
 	return (ret);
 }
 
@@ -119,19 +98,22 @@ int		get_next_line(int const fd, char **line)
 	ret = 0;
 	if (!infos)
 		infos = ft_memalloc(sizeof(infos));
-	if (!infos->count)
-		infos->count = 0;
 	if (!infos->start)
 		infos->start = 0;
+	if (!infos->end)
+		infos->end = 0;
+	if (infos->end == 1)
+	{
+		ft_free_all(infos);
+		return (0);
+	}
 	if (BUFF_SIZE < 1)
 		return (-1);
 	if (infos->tmp == NULL)
 		infos->tmp = (char *)ft_memalloc(sizeof(char) * BUFF_SIZE);
 	if (infos->state == 0)
-		ret = ft_read_fd(fd, infos, line);
+		ret = ft_read_fd(fd, infos);
 	if (ret >= 0)
 		ret = ft_write_inline(line, infos);
-	if (ret >= 0 && infos->count <= infos->linenu)
-		ret = 1;
 	return (ret);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -5,32 +5,29 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bbecker <bbecker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/10 09:51:47 by bbecker           #+#    #+#             */
-/*   Updated: 2014/11/12 16:00:59 by bbecker          ###   ########.fr       */
+/*   Created: 2014/11/13 17:10:47 by bbecker           #+#    #+#             */
+/*   Updated: 2014/11/13 17:13:13 by bbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 100000
 
 # include "libft.h"
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <unistd.h>
 
+int	get_next_line(int const fd, char **line);
 
-typedef	struct		s_infos
+typedef struct	s_infos
 {
 	int		start;
-	int		count;
 	char	*tmp;
 	int		state;
-	int		linenu;
-	
-}					t_infos;
-
-int get_next_line(int const fd, char **line);
+	int		end;
+}				t_infos;
 
 #endif
